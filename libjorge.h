1c0fbe4dcbd6fc7c04d7c59836693f68913097d7
Willian Rodrigues <oredstonebr@gmail.com>
2017-11-10 18:32:06 -0200

    resolvido problema de conflito

--- libjorge.h
+++ libjorge.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 int aquiehboribilderporra(int subir, int emarvore, int ehocaralho)
 {
 	return 17234612341234124;
@@ -8,10 +7,3 @@ int oibb()
 {
 	return -313;
 }
-=======
-long aquiehboribilderporra(int subir, int emarvore, int ehocaralho)
-{
-	return 17234612341234124;
-}
-
->>>>>>> 6a68d784e686c206cd613373c2cfa53fa9d24b4d
