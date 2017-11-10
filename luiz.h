7eb58b088307db36c71f3dd7d15c83c33ec857e9
LuizHenriqueBO <luiz_hickbarros@hotmail.com>
2017-11-10 19:00:04 -0200

    soma

--- luiz.h
+++ luiz.h
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
-int luizFuncaoSoma(int a,int c){
-    return (a+c);
-}
-=======
-
-int luizFuncaoSoma(int a,int b) {
+int Soma(int a,int b) {
     return (a+b);
 }
 
->>>>>>> ec34f8edd2baaa98e5040ff76210ff2e715b21b2
